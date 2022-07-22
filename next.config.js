44060c650256c2a521f877891bd2620c376c6b0a
loque18 <madridx2010@gmail.com>
2022-07-22 13:06:14 -0500

    feat: added arch diagram part1

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     reactStrictMode: true,
-    i18n: {
-        locales: ['en'],
-        defaultLocale: 'en',
-    },
+    // i18n: {
+    //     locales: ['en'],
+    //     defaultLocale: null,
+    // },
     env: {
         FONT_AWESOME_KEY: '8d70729523',
     },
