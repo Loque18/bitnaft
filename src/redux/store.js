afcddd6674513b8752be9a1c74966c043244515b
loque18 <madridx2010@gmail.com>
2022-07-25 11:59:45 -0500

    style: added material tokens as scss variables

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,18 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 
-import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 
 import rootSaga from './sagas';
 
-import exampleReducer from './reducers/exampleReducer';
 import modalReducer from './reducers/modalReducer';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const reducer = combineReducers({
-    exampleReducer,
     modalReducer,
 });
 
-const store = createStore(reducer, applyMiddleware(thunk, sagaMiddleware));
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(rootSaga);
 
 export default store;
