e47333324db4c837b1d03d5c0541eadddaef3782
loque18 <madridx2010@gmail.com>
2022-07-27 18:15:50 -0500

    feat: added login page

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,8 +1,10 @@
 import { getLayout as getPageTitleLayout } from 'src/layouts/page-title';
 
-import styles from './box.module.scss';
+import LoginForm from 'src/sub-components/login/form';
 
-const { box_container, box_column } = styles;
+import styles from 'src/scss/common_modules/box.module.scss';
+
+const { box_container, box_column, is_reverse } = styles;
 
 const LoginPage = () => {
     return (
@@ -12,37 +14,17 @@ const LoginPage = () => {
                     className="container is-max-desktop py-6 px-5"
                     style={{ height: '100%', display: 'grid', placeItems: 'center' }}
                 >
-                    <div className={`box p-0 ${box_container}`} style={{ height: '100%', maxHeight: '100%' }}>
-                        <div
-                            className={` p-5 ${box_column}`}
-                            style={{
-                                height: '100%',
-                                display: 'grid',
-                                placeItems: 'center',
-                            }}
-                        ></div>
-                        <div className={`has-bg-md-ref-primary-30 p-5 ${box_column}`}>
+                    <div
+                        className={`box p-0 ${box_container} ${is_reverse}`}
+                        style={{ height: '100%', maxHeight: '100%' }}
+                    >
+                        <div className={`has-bg-md-ref-primary-30 p-5 ${box_column} `}>
                             <div
                                 className="col-container "
                                 style={{ height: '100%', display: 'grid', placeItems: 'center' }}
                             >
                                 <div>
-                                    <img src="media/pages/signup/bg-img.webp" alt="" />
-                                    <div className="has-text-centered mt-5">
-                                        <img src="media/logos/logo-light.png" alt="" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* <div className={`box  p-0 ${box_container}`} style={{ height: '100%', maxHeight: '100%' }}>
-                        <div className={`has-bg-md-ref-primary-30 p-5 ${box_column}`}>
-                            <div
-                                className="col-container "
-                                style={{ height: '100%', display: 'grid', placeItems: 'center' }}
-                            >
-                                <div>
-                                    <img src="media/pages/signup/bg-img.webp" alt="" />
+                                    <img src="media/pages/login/bg.webp" alt="" width="50%" />
                                     <div className="has-text-centered mt-5">
                                         <img src="media/logos/logo-light.png" alt="" />
                                     </div>
@@ -57,10 +39,20 @@ const LoginPage = () => {
                                 placeItems: 'center',
                             }}
                         >
-                      
-                            <SignupForm />
+                            <div>
+                                <section className="mb-5">
+                                    <h1 className="title is-4 has-text-hblue has-text-centered">Welcome back!</h1>
+                                    <p className="has-text-centered">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis
+                                        mauris.{' '}
+                                    </p>
+                                </section>
+                                <section>
+                                    <LoginForm />
+                                </section>
+                            </div>
                         </div>
-                    </div> */}
+                    </div>
                 </div>
             </div>
         </div>
--- src/sub-components/login/form/index.js
+++ src/sub-components/login/form/index.js
@@ -1,7 +1,20 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import Link from 'next/link';
+import { useState } from 'react';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 
+import styles from 'src/scss/common_modules/form_utils.module.scss';
+
+const { eye_button } = styles;
+
+const Eye = () => <i className="fa-solid fa-eye has-text-md-ref-primary-30" />;
+const EyeSlash = () => <i className="fa-solid fa-eye-slash has-text-md-ref-primary-30" />;
+
 const Form = () => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [loading, setLoading] = useState(false);
+
     const formik = useFormik({
         initialValues: {
             email: '',
@@ -16,7 +29,93 @@ const Form = () => {
         },
     });
 
-    return <form onSubmit={formik.handleSubmit}>asd</form>;
+    const changePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <div className="field">
+                <label className="label is-size-7">Email</label>
+                <div className="control has-icons-left">
+                    <input
+                        className={`input ${formik.errors.email && formik.touched.email ? 'is-danger' : ''}`}
+                        type="email"
+                        placeholder="Email"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        name="email"
+                    />
+
+                    <span className="icon is-small is-left">
+                        <i className="fas fa-at" />
+                    </span>
+
+                    <p className="help is-danger">
+                        {formik.touched.email && formik.errors.email ? formik.errors.email : ''}
+                    </p>
+                </div>
+            </div>
+
+            <div className="field">
+                <label className="label is-size-7">Password</label>
+                <div className="control has-icons-left has-icons-right">
+                    <input
+                        className={`input ${formik.touched.password && formik.errors.password ? 'is-danger' : ''}`}
+                        type={passwordVisible ? 'text' : 'password'}
+                        placeholder="Password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        name="password"
+                    />
+
+                    <span className="icon is-small is-left">
+                        <i className="fa-solid fa-lock" />
+                    </span>
+
+                    {formik.touched.password && formik.errors.password ? (
+                        <p className="help is-danger">{formik.errors.password}</p>
+                    ) : (
+                        '⠀'
+                    )}
+
+                    <button
+                        aria-label={passwordVisible ? 'Hide password' : 'Show password'}
+                        className={`unstyled-button ${eye_button}`}
+                        type="button"
+                        onClick={changePasswordVisibility}
+                        style={{ zIndex: '5' }}
+                        tabIndex="-1"
+                    >
+                        <span className="icon is-small">{passwordVisible ? <EyeSlash /> : <Eye />}</span>
+                    </button>
+                </div>
+            </div>
+
+            <div className="field">
+                <div className="control">
+                    <button
+                        aria-label="Log in"
+                        className={`button is-hblue is-fullwidth ${loading ? 'is-loading' : ''}`}
+                        type="submit"
+                    >
+                        Login
+                    </button>
+                </div>
+                <br />
+                <div className="has-text-centered">
+                    Not a member yet?{' '}
+                    <Link href="/signup" passHref>
+                        <a href="replace" className="has-text-md-source-primary">
+                            <u>Create an account</u>
+                        </a>
+                    </Link>
+                </div>
+            </div>
+        </form>
+    );
 };
 
 export default Form;
